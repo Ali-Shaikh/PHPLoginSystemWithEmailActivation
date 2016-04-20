8e46694ef6051b1c6728686a82b4c84cd39f0aac
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2016-04-20 02:48:02 +0100

    Basic Structure. Created includes with footer, header and nav files. Also created index file.

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+</div> <!-- /container -->
+
+<!-- JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery.js"><\/script>')</script>
+<script src="js/bootstrap.js"></script>
+<script src="js/scripts.js"></script>
+
+</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ali Shaikh on 20/04/2016.
+ */
